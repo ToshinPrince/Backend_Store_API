80feab47bc02b9387ef6d71aa1ee5d7bb218784d
Toshin Prince <toshinprince@gmail.com>
2023-11-20 14:12:35 +0530

    connectDB connecting to mongoDB

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 //async error
 
 const express = require("express");
@@ -7,25 +7,30 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+const connectDB = require("./db/connect");
+
 //Middleware
 app.use(express.json());
 
-//rootes
+//routes
 
 app.get("/", (req, res) => {
   res.send(`<h1>API Store</h1><a href="api/v1/products">Products Route</a>`);
 });
+
+app.get("api/v1/peoducts");
 
 //products Route
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     //Connect DB
+    await connectDB(process.env.MONGO_URI);
 
     app.listen(port, () => console.log(`Server is listning on port ${port}`));
   } catch (error) {
