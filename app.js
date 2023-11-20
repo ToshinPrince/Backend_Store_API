940d52f67b9d0a93e7558d9bd6e404f71eb4d19c
Toshin Prince <toshinprince@gmail.com>
2023-11-20 12:45:01 +0530

    Setting up express

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require("dotenv").config;
+//async error
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//Middleware
+app.use(express.json());
+
+//rootes
+
+app.get("/", (req, res) => {
+  res.send(`<h1>API Store</h1><a href="api/v1/products">Products Route</a>`);
+});
+
+//products Route
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    //Connect DB
+
+    app.listen(port, () => console.log(`Server is listning on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
