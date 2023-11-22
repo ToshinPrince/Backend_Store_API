46a5effe7b66ac52a8bf884bb34769132aea795e
Toshin Prince <toshinprince@gmail.com>
2023-11-22 11:59:29 +0530

    comntrollers query

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,27 @@
+const Products = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: `Products testing route` });
+  const products = await Products.find({ name: "high-back bench" });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: `Products Route` });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    //The $regex operator in MongoDB is used for pattern matching within queries.
+    queryObject.name = { $regex: name };
+  }
+
+  console.log(queryObject);
+  const products = await Products.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
