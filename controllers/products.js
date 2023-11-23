982d91aef2a2acbb2188a61ab77e575ae15a039d
Toshin Prince <toshinprince@gmail.com>
2023-11-23 12:23:03 +0530

    controllers- sort, fields(select)

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,11 @@
 const Products = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  const products = await Products.find({ name: "high-back bench" });
+  const products = await Products.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,7 +20,22 @@ const getAllProducts = async (req, res) => {
   }
 
   console.log(queryObject);
-  const products = await Products.find(queryObject);
+  let result = Products.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
